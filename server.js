c575453dbfe3ed7b6e730ae6ecc2acf4a6fc4dff
SIDDHANT VINCHURKAR <you@example.com>
2021-02-02 02:56:36 +0530

    fixing

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
-const knex = require('knex'
-)
+const knex = require('knex');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connectionString: process.env.DATABASE_URL,
-    ssl: false
+    ssl: true
 });
 
 const app = express();
